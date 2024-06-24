22a2b8a71ab8e5c0d50d78205b09476ca5c18d4b
huangjoseph45 <huangjoseph45@gmail.com>
2024-06-24 15:38:46 -0400

    menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import "../styles/components/header.css";
-import "../styles/components/hero.css";
-import "../styles/components/footer.css";
+import "../styles/header.css";
+import "../styles/index__components/hero.css";
+import "../styles/index__components/footer.css";
+import "../styles/menu__components/intro.css"
+import "../styles/menu__components/menu__items.css"
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/utils.css";
